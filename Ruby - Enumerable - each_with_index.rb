e86a74493c87845684efc6939459478d68352f17
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-07 08:31:47 +0530

    Create Ruby - Enumerable - each_with_index.rb

---
+++ Ruby - Enumerable - each_with_index.rb
@@ -0,0 +1,9 @@
+def skip_animals(animals, skip)
+    arr = []
+    animals.each_with_index do |item, index|
+        if(index >= skip)
+            arr.push("#{index}:#{item}")
+        end
+    end
+    return arr
+end
