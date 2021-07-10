20f84701c28fef8057c1070da5a7800b2b8e9de6
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-10 17:46:32 +0530

    Update Partial Applications.rb

--- Partial Applications.rb
+++ Partial Applications.rb
@@ -1,10 +1,14 @@
-#combination = first_value! / second_value! * (first_value - second_value)!
-combination = ->(first_value) do
-    ->(second_value) do
-        term1 = (1..second_value).reduce(:*)
-        term2 = (1..(first_value-second_value)).reduce(:*)
-        divisor = term1*term2
-        operator = (1..first_value).reduce(:*)
-        operator/divisor
+def fact(x)
+    (1..x).inject(:*) || 1
+end
+
+combination = -> (n) do
+    -> (r) do
+        fact(n) / (fact(n-r) * fact(r))
     end
 end
+
+n = gets.to_i
+r = gets.to_i
+nCr = combination.(n)
+puts nCr.(r)
