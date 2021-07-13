a2ea6e70ee9215a02f52a857130344c003417b7c
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-13 23:58:45 +0530

    Update Ruby - Strings - Methods II.rb

---
+++ Ruby - Strings - Methods II.rb
@@ -0,0 +1,11 @@
+def strike(word)
+    "<strike>#{word}</strike>"
+  end
+  
+  def mask_article(string, words)
+    words.each do |word|
+      string.gsub!(word, strike(word))
+    end
+  
+    string
+  end
